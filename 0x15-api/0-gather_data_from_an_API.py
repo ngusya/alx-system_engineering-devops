1ff3b78307d4872d9e8ed6c3fb3885606ccfe37d
Ubuntu <ubuntu@Ubuntu.myguest.virtualbox.org>
2023-11-13 13:57:07 +0300

    Pep8 check

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-# Script that, using this REST API, for a given employee ID,
-# returns information about his/her
-# todo list progress
+"""
+ Script that, using this REST API, for a given employee ID,
+ returns information about his/her
+ todo list progress
+ """
 
 import requests
 from sys import argv

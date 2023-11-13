caf402e65357d5944b548b93b275fccc51930884
Ubuntu <ubuntu@Ubuntu.myguest.virtualbox.org>
2023-11-13 14:11:28 +0300

    update task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -18,11 +18,19 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos?userId={}".format(ID)).json()
-    tasks = []
+    
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
+
+    """
+    completed = []
     for task in todos:
         if task.get('completed') is True:
             tasks.append(task.get('title'))
     print("Employee {} is done with tasks({}/{}):".
-          format(user.get('name'), len(tasks), len(todos)))
+          format(user.get('name'), len(completed), len(todos)))
     for task in tasks:
         print("\t {}".format(task))
+        """
